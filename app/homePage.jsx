62d0ee7c0458ae58c9c9c84f6276025ce92a4b29
juliapariz <julia.pariz@aluno.senai.br>
2026-08-07 10:30:16 -0300

    Início do projeto

--- app/homePage.jsx
+++ app/homePage.jsx
@@ -1,61 +1,19 @@
-import styles from "./page.module.css";
 import Image from "next/image";
 import Header from "./components/header";
 import Footer from "./components/footer";
-import Banner from "./img/banner.png";
+import Banner from "./fotos/foto.jpg";
 
-
-export default function HomePage(){
-
-    return(
-
-        <div className={styles.container}>
-
-            <section className={styles.hero}>
-
-                <h1>Bem-vindo ao meu Site</h1>
-
-                <p>
-                    Projeto desenvolvido pelo 3B
-                </p>
-
-                <button className={styles.botao}>
-                    Saiba Mais
-                </button>
-
-            </section>
-
-            <section className={styles.cards}>
-
-                <div className={styles.card}>
-                    <h2>HTML</h2>
-                    <p>
-                        Estrutura do site.
-                    </p>
-                </div>
-
-                <div className={styles.card}>
-                    <h2>CSS</h2>
-                    <p>
-                        Responsável pelo design.
-                    </p>
-                </div>
-
-                <div className={styles.card}>
-                    <h2>JavaScript</h2>
-                    <p>
-                        Responsável pela interatividade.
-                    </p>
-                </div>
-
-            </section>
-
-            <footer className={styles.footer}>
-                © 2026 - Meu Site
-            </footer>
-
-        </div>
-
+export default function HomePage() {
+return (
+        <>
+            <Header />
+            <main>
+                <section>
+                    <Image src={Banner} alt="Banner" />
+                </section>
+                <p>Free Fire</p>
+            </main>
+            <Footer />
+        </>
     )
-
 }
--- app/page.js
+++ app/page.js
@@ -1,23 +1,7 @@
-import styles from "./page.module.css";
-import Image from "next/image";
-import Header from "./components/header";
-import Footer from "./components/footer";
+import HomePage from './homePage';
 
 export default function Home() {
   return (
-    <>
-      <Header />
-
-      <main>
-        <Image
-          src="/banner.png"
-          alt="Banner do Terceirão"
-          width={1536}
-          height={1024}
-        />
-      </main>
-
-      <Footer />
-    </>
-  );
+      <HomePage />
+  )
 }
